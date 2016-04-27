2ba6ff4df9c9877f4aa6cfe123d6f87593f06266
Jose Ventura <jose.ventura@horustm.com.br>
2016-04-27 09:43:57 -0300

    bugfixes cli-table

--- bin/table.js
+++ bin/table.js
@@ -1,38 +1,30 @@
 var _ = require('lodash'),
-    Table = require('cli-table')
-
-var emptyChars = {
-   'top': '' , 'top-mid': '' , 'top-left': '' ,
-   'top-right': '', 'bottom': '' , 'bottom-mid': '' ,
-   'bottom-left': '' , 'bottom-right': '', 'left': '' ,
-   'left-mid': '' , 'mid': '' , 'mid-mid': '',
-   'right': '' , 'right-mid': '' , 'middle': ''
- }
+    chalk = require('chalk'),
+    table = require('table')
 
 exports.display = function(opts, rows) {
-  var table = new Table({
-    head: opts.head,
-    chars: emptyChars,
-    colWidths: opts.colWidths
-  })
+  var data = []
+  data.push(opts.head.map(function(h) { return chalk.green(h) }))
 
   rows.forEach(function(row) {
-    var data = row
+    var tabRow = opts.pick.map(function(prop) {
+      if ( _.isFunction(prop) ) {
+        return prop(row) || 'null'
+      } else {
+        return _.get(row, prop, 'null')
+      }
+    })
 
-    if ( opts.pick ) {
-      data = opts.pick.map(function(prop) {
-        if ( _.isFunction(prop) ) {
-          return prop(row) || 'null'
-        } else {
-          return _.get(data, prop, 'null')
-        }
-      })
-    }
-
-    table.push(data)
+    data.push(tabRow)
   })
 
-  console.log(table.toString())
+  var output = table.default(data, {
+    border: table.getBorderCharacters('void'),
+    columnDefault: { paddingLeft: 1, paddingRight: 1 },
+    drawHorizontalLine: function() { return false }
+  })
+
+  console.log(output)
   if ( _.get(opts, 'showCount', true) )
     console.log(' ' + rows.length + ' rows.')
 }
@@ -41,23 +33,23 @@ exports.tablify = function(opts) {
   return _.partial(exports.display, opts)
 }
 
-exports.detailify = function(opts) {
-  return function(obj) {
-    var table = new Table({
-      chars: emptyChars
-    })
-
-    _.forOwn(opts.pick, function(val, key) {
-      var row = {}
-      if ( _.isFunction(val) ) {
-        row[key] = val(obj)
-      } else if ( _.isString(val) ) {
-        row[key] = _.get(obj, val, 'null')
-      }
-
-      table.push(row)
-    })
-
-    console.log(table.toString())
-  }
-}
+// exports.detailify = function(opts) {
+//   return function(obj) {
+//     var table = new Table({
+//       chars: emptyChars
+//     })
+//
+//     _.forOwn(opts.pick, function(val, key) {
+//       var row = {}
+//       if ( _.isFunction(val) ) {
+//         row[key] = val(obj)
+//       } else if ( _.isString(val) ) {
+//         row[key] = _.get(obj, val, 'null')
+//       }
+//
+//       table.push(row)
+//     })
+//
+//     console.log(table.toString())
+//   }
+// }
